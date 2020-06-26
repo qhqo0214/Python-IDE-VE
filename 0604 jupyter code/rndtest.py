48d8dc5ee2c17ff0f9485bc4d324cbf2574a3f9b
qhqo0214 <64951085+qhqo0214@users.noreply.github.com>
2020-06-26 10:11:01 +0900

    f

--- 0604 jupyter code/rndtest.py
+++ 0604 jupyter code/rndtest.py
@@ -3,9 +3,3 @@ import random as rd
 print(rd.random())
 print(rd.randint(1, 3))
 print(rd.choice(range(10)))
-
-print(rd.choices(range(30), k=10))
-print(rd.choices(range(30), k=10))
-print(rd.choices(range(30), k=10))
-print(rd.choices(range(30), k=10))
-print(rd.choices(range(30), k=10))
